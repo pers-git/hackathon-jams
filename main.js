9697d2002eb6864f9d0fcd9b538cdd9b5a118942
rainysnowman <neohibikiedits@gmail.com>
2025-09-09 15:40:36 +1000

    Working buttons :D

--- main.js
+++ main.js
@@ -7,3 +7,23 @@ const likeliness = Object.freeze({
 var rating = likeliness.LIKELY;
 const myResult = document.getElementById("likeliness-result");
 myResult.textContent = rating;
+
+
+function upload() { 
+  const cancel = document.getElementById('cancel');
+  const confirm = document.getElementById('confirm');
+  const image = document.getElementById('image');
+
+  image.style.visibility = "visible";
+  cancel.style.visibility = "visible";
+  confirm.style.visibility = "visible";
+}
+
+function cancel_upload() {
+  const cancel = document.getElementById('cancel');
+  const confirm = document.getElementById('confirm');
+  const image = document.getElementById('image');  
+  image.style.visibility = "hidden";
+  cancel.style.visibility = "hidden";
+  confirm.style.visibility = "hidden";
+}
